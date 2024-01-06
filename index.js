5f2d19895d575c7c65928ea1355bcf030653ca05
nick amaro <nickamaro@nicks-MacBook-Pro.local>
2024-01-05 19:27:13 -0600

    got monog to work

--- index.js
+++ index.js
@@ -18,3 +18,37 @@ app.use('/api', apiRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+// index.js
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB');
+  });
+  
+  mongoose.connection.on('error', (err) => {
+    console.error(`MongoDB connection error: ${err}`);
+  });
+  
+  mongoose.connection.on('disconnected', () => {
+    console.log('Disconnected from MongoDB');
+  });
+
+  // index.js
+
+mongoose.connect('mongodb://localhost:27017/your-database', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // Other options
+  });
+
+  // index.js
+
+process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+      console.log('MongoDB connection closed due to app termination');
+      process.exit(0);
+    });
+  });
+  
+  
+  
